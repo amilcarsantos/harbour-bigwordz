0f11c40069fc4fae0637ff8a2dda4b1a21f0bb35
TylerTemp <huangling.exe@gmail.com>
2014-11-24 10:43:09 +0800

    add Chinese Simplfied(zh_CN) translation

---
+++ translations/harbour-bigwordz-zh.ts
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="84"/>
+        <source>Displays words on the screen with large letters.</source>
+        <translation>在屏幕上以大号字显示文字</translation>
+    </message>
+</context>
+<context>
+    <name>FavoritesZone</name>
+    <message>
+        <location filename="../qml/pages/util/FavoritesZone.qml" line="72"/>
+        <source>Remove from favorites</source>
+        <translation>从收藏夹移除</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="97"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="101"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="107"/>
+        <source>Toogle full screen</source>
+        <translation>全屏开关</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="124"/>
+        <source>Unlock orientation</source>
+        <translation>解锁方向锁定</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="124"/>
+        <source>Lock orientation</source>
+        <translation>锁定方向</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="137"/>
+        <source>Stored words</source>
+        <translation>已保存的文字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="147"/>
+        <source>Store current</source>
+        <translation>保存当前</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="89"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="90"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="93"/>
+        <source>Appearance</source>
+        <translation>外观</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="100"/>
+        <source>Text colors</source>
+        <translation>文字颜色</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="107"/>
+        <source>Theme colors</source>
+        <translation>主题颜色</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="116"/>
+        <source>Black and white</source>
+        <translation>黑底白字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="125"/>
+        <source>Customize</source>
+        <translation>自定义</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="134"/>
+        <source>Text color</source>
+        <translation>文字颜色</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="141"/>
+        <source>Background</source>
+        <translation>背景</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="146"/>
+        <source>General</source>
+        <translation>总体</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="150"/>
+        <source>Start with latest stored word</source>
+        <translation>自上次保存文字开始</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="151"/>
+        <source>Next restart the initial word is the latest stored word</source>
+        <translation>启动时自动显示上次储存的文字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="156"/>
+        <source>Store words automatically</source>
+        <translation>自动显示文字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="157"/>
+        <source>Store words when switching to full screen or only by the pull up menu</source>
+        <translation>在切换到全屏时或仅从下拉菜单选择时保存文字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="162"/>
+        <source>Double tap to toggle full screen</source>
+        <translation>双击开关全屏</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="163"/>
+        <source>Double tap over the words to swicth between edit mode and full screen</source>
+        <translation>双击文字来切换编辑模式和全屏模式</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="168"/>
+        <source>Toogle full screen with sensors</source>
+        <translation>通过传感器开关全屏</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="169"/>
+        <source>Set the screen vertically to switch from edit mode to full screen</source>
+        <translation>横屏时自动从编辑模式转换到全屏模式</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="186"/>
+        <source>Slowest</source>
+        <translation>最慢</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="189"/>
+        <source>Slower</source>
+        <translation>较慢</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="192"/>
+        <source>Fastest</source>
+        <translation>最快</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="195"/>
+        <source>Faster</source>
+        <translation>较快</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="197"/>
+        <source>Default</source>
+        <translation>默认</translation>
+    </message>
+</context>
+<context>
+    <name>StoredWordsPage</name>
+    <message>
+        <location filename="../qml/pages/StoredWordsPage.qml" line="57"/>
+        <source>Stored words</source>
+        <translation>已保存的文字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoredWordsPage.qml" line="92"/>
+        <source>Delete all</source>
+        <translation>删除所有</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoredWordsPage.qml" line="97"/>
+        <source>Deleting all stored words</source>
+        <translation>删除所有已保存文字</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoredWordsPage.qml" line="169"/>
+        <source>Deleting</source>
+        <translation>正在删除</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoredWordsPage.qml" line="197"/>
+        <source>Remove from favorites</source>
+        <translation>移出收藏夹</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoredWordsPage.qml" line="197"/>
+        <source>Add to favorites</source>
+        <translation>添加到收藏夹</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StoredWordsPage.qml" line="208"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+</context>
+</TS>
